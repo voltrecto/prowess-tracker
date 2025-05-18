7085310e18d6d22a28f646a11f56ba4d0e0f4fc8
voltrecto <voltairerecto@gmail.com>
2025-05-18 10:43:32 -0500

    Add main menu

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # prowess-tracker
+
+Working on a simple prowess tracker for MTG Commander. Starting with CLI then will convert to an app later once logic is completed.
---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Prowess Tracker")
+    print("Select option:")
+    print("1. New turn/reset all")
+    print("2. Undo last action")
+    print("3. You cast a noncreature spell (instant or sorcery)")
+    print("4. You cast a noncreature spell (other)")
+    print("5. You cast a creature spell")
+    print("6. Opponent cast (any) spell")
+
+if __name__ == "__main__":
+    main()
